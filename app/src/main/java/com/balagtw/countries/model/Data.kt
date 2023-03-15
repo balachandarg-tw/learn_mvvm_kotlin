2fe686f6c7f685c1ed4a3b28d597c4a6f4d7fe2e
balachandarg-tw <balachandar.g@thoughtworks.com>
2023-03-15 09:58:08 +0530

    Initial commit

---
+++ app/src/main/java/com/balagtw/countries/model/Data.kt
@@ -0,0 +1,3 @@
+package com.balagtw.countries.model
+
+data class Country(val countryName: String?)
---
+++ app/src/main/java/com/balagtw/countries/viewmodel/ListViewModel.kt
@@ -0,0 +1,6 @@
+package com.balagtw.countries.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel: ViewModel() {
+}
