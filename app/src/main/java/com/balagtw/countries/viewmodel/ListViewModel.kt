990e6f1940ceca3e102071404731e6478de9a64d
balachandarg-tw <balachandar.g@thoughtworks.com>
2023-03-15 10:37:08 +0530

    Implement ViewModel

--- app/src/main/java/com/balagtw/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/balagtw/countries/viewmodel/ListViewModel.kt
@@ -1,6 +1,34 @@
 package com.balagtw.countries.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.balagtw.countries.model.Country
 
 class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(Country("India"),
+            Country("US"),
+            Country("Italy"),
+            Country("Canada"),
+            Country("England"),
+            Country("Australia"),
+            Country("Pakistan"),
+            Country("UK"),
+            Country("Srilanka"),
+            )
+
+        countryLoadError.value = false
+        loading.value = false
+
+        countries.value = mockData
+    }
 }
