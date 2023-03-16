56736b61bc2e3cdf9d092f5d167e41d4d3876f4c
balachandarg-tw <balachandar.g@thoughtworks.com>
2023-03-16 11:02:47 +0530

    Implement View for MVVM

--- app/src/main/java/com/balagtw/countries/view/MainActivity.kt
+++ app/src/main/java/com/balagtw/countries/view/MainActivity.kt
@@ -2,11 +2,63 @@ package com.balagtw.countries.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.balagtw.countries.R
+import com.balagtw.countries.databinding.ActivityMainBinding
+import com.balagtw.countries.viewmodel.ListViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var viewModel: ListViewModel
+    private val countriesAdapter = CountryListAdapter(arrayListOf())
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.recyclerViewCountries.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.countries.observe(this, Observer { countries ->
+            countries?.let {
+                countriesAdapter.updateCountries(it)
+            }
+        })
+
+        viewModel.countryLoadError.observe(this, Observer{isError ->
+            isError?.let {
+                binding.tvError.visibility = if(it) View.VISIBLE else View.GONE
+            }
+
+        })
+
+        viewModel.loading.observe(this, Observer{isLoading ->
+            isLoading?.let {
+                binding.loadingView.visibility = if(it) View.VISIBLE else View.GONE
+
+                if(it){
+                    binding.recyclerViewCountries.visibility = View.GONE
+                    binding.tvError.visibility = View.GONE
+                }
+            }
+
+        })
     }
 }
